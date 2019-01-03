c3cab6499e7539c97f0babcae4e5beb8042e6040
Chu(朱恆慶 <chicochu9333@gmail.com>
2019-01-03 17:11:10 +0800

    push

---
+++ GetPostExample/GetPostExample/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GetPostExample.Models;
+namespace GetPostExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DateTime date = DateTime.Now;
+            ViewBag.Date = date;
+
+            Student data = new Student("1", "小明", 80);
+            return View(data);
+        }
+
+        public ActionResult Transcripts(string id, string name, int score)
+        {
+            Student data = new Student(id, name, score);
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Transcripts(FormCollection post)
+        {
+            string id = post["id"];
+            string name = post["name"];
+            int score = Convert.ToInt32(post["score"]);
+            Student data = new Student(id, name, score);
+            return View(data);
+        }
+    }
+}
---
+++ GetPostExample/GetPostExample/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetPostExample.Models
+{
+    public class Student
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+        public Student()
+        {
+            id = string.Empty;
+            name = string.Empty;
+            score = 0;
+        }
+        public Student(string _id, string _name, int _score)
+        {
+            id = _id;
+            name = _name;
+            score = _score;
+        }
+        public override string ToString()
+        {
+            return $"學號:{id}, 姓名:{name}, 分數:{score}.";
+        }
+    }
+}
